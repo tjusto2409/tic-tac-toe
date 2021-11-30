70d8d7d922749e7c867072e7a08ef070c5dd1e9b
Thiago Justo <thiago.justo@stone.com.br>
2021-11-30 11:07:08 -0300

    Add .env by project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express'),
 app = express(),
 boardRouter = require('./src/router/boardRouter');
+require('dotenv/config');
 
-app.use(boardRouter);
-app.listen(3000)
+console.log(process.env.PORT)
+app.listen(process.env.PORT)
